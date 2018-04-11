f478ec11bed580e82ad07c5e2d5e9f66f1b13ce8
Dennis Weber <dennis.weber@actico.com>
2018-04-11 13:16:20 +0200

    Add comments about available variables

--- java/src/animals/DefaultWolf.java
+++ java/src/animals/DefaultWolf.java
@@ -3,6 +3,14 @@ package animals;
 // Optional code here
 public class DefaultWolf extends Animal {
     // Optional code here
+
+    // Available variables:
+    // - surroundings    A zero indexed, 3 by 3 matrix of characters that represent nearby animals.
+    //                   Empty tiles are represented by a space character (' '). You are at surroundings[1][1].
+    //                   For example, to your right would be surroundings[1][2], and above you is surroundings[0][1].
+    //                   Your surroundings are updated just before being asked to move,
+    //                   but may be out of date when asked to fight.
+    // - MAP_SIZE        The size of the map, as a int
     public DefaultWolf() {
         super('W');
         /* Optional code here */
