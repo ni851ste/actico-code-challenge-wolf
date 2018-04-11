e3d04b450c8200642c996240ba87be5346656d4b
Dennis Weber <dennis.weber@actico.com>
2018-04-11 12:25:24 +0200

    Add default code from Stackoverflow question

---
+++ java/src/wild/Wild.java
@@ -0,0 +1,49 @@
+
+package wild;
+
+import animals.*;
+import java.awt.*;
+import javax.swing.*;
+
+public class Wild {
+    
+    private static final Class[] classes = { Bear.class, Lion.class, Stone.class, Wolf.class };
+    public static final int MAP_SIZE = Math.round((float)Math.sqrt(classes.length+3)*20);;
+
+    public static void main(String[] args) {
+        
+        int size = Math.round((float)Math.sqrt(classes.length+3)*20);
+        Game game = new Game(size);
+        
+        Statistics stats = new Statistics(game, classes);
+        
+        for(Class c : classes)
+            game.populate(c, 100);
+        stats.update();
+        
+        JFrame gui = new JFrame();
+        Container pane = gui.getContentPane();
+        
+        JLabel boardLabel = new JLabel();
+        boardLabel.setFont(new Font(Font.MONOSPACED, Font.PLAIN, 12));
+        boardLabel.setText(game.toString());
+        pane.add(boardLabel, BorderLayout.WEST);
+        
+        JLabel statsLabel = new JLabel();
+        statsLabel.setFont(new Font(Font.MONOSPACED, Font.PLAIN, 12));
+        statsLabel.setText(stats.toString());
+        pane.add(statsLabel, BorderLayout.EAST);
+        
+        gui.pack();
+        gui.setVisible(true);
+        
+        while(true) {
+            game.iterate();
+            stats.update();
+            boardLabel.setText(game.toString());
+            statsLabel.setText(stats.toString());
+            try { Thread.sleep(100); } catch (InterruptedException e) {}
+        }
+    }
+    
+}
